69604198dd7251e246e13ff4563f826fd5dc7159
Pedro Henrique Carvalho dos Reis <phenrique.carvalho.reis@gmail.com>
2021-03-17 23:10:37 +0100

    add final spaces

--- src/functions.py
+++ src/functions.py
@@ -8,3 +8,4 @@ def updateAllowedK(D, howManyDependancies, nextTask, allowed):
         howManyDependancies[task] -= 1
         if howManyDependancies[task] == 0:
             allowed.add(task)
+            
