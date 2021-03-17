a094eb47836635868575c8a972361c3c5ea56635
Eylul Yalcinkaya <eylulyalcnkaya@gmail.com>
2021-03-17 22:28:17 +0100

    Implemented initializeAnt function

--- src/main.py
+++ src/main.py
@@ -1,25 +1,58 @@
+import random
+
 def exec(jsonPath, numberOfAnts, numberOfProcessors, iterMax, alpha, beta, rho):
- 	D, ET, initialAllowed, numberOfTasks = initializeDependancyAndExecutionTimeMatrizes(jsonPath) # Matheus
- 	eta = definingEta(ET) # Matheus
- 	pheromone = initializePheromone(ET) # Matheus
+    D, ET, initialAllowed, numberOfTasks = initializeDependancyAndExecutionTimeMatrizes(jsonPath) # Matheus
+    eta = definingEta(ET) # Matheus
+    pheromone = initializePheromone(ET) # Matheus
 
- 	# The solution
- 	x = np.ndarray((numberOfTasks, numberOfProcessors), int)
- 	L = float("inf")
+    # The solution
+    x = dict([])
+    '''
+    x = {
+        processorId : [taskId] 
+    }
+    '''
+    L = float("inf")
 
- 	for iter in range(iterMAX):
+    for iter in range(iterMAX):
 
- 		for ant in numberOfAnts:
- 			allowed = initialAllowed
- 			antX = initializeAnt(initialAllowed) # Eylul
+        for ant in numberOfAnts:
+            allowed = initialAllowed
+            taskId, antX = initializeAnt(allowed, numberOfProcessors) # Eylul
+            updateAllowedK(D, howManyDependencies, taskId, allowed)
 
- 			for task in numberOfTasks:
- 				nextTask, nextProcessor = selectTheNextRoute(eta, alpha, pheromone, beta, allowed, antX) # Theodore and Pedro
- 				updateAllowedK() # Theodore and Pedro
- 				antX[nextTask, nextProcessor] = 1
+            for task in (numberOfTasks - 1):
+                nextTask, nextProcessor = selectTheNextRoute(eta, alpha, pheromone, beta, allowed, antX) # Theodore and Pedro
+                updateAllowedK() # Theodore and Pedro
+                antX[nextTask, nextProcessor] = 1
 
- 			antL = costfunction(antX, ET) # Eylul
- 			if antL < L:
- 				x = antX
- 				L = antL
- 			updatePheromone(pheromone, rho, antX, ET) # Yasmine 
+            antL = costfunction(antX, ET) # Eylul
+            if antL < L:
+                x = antX
+                L = antL
+            updatePheromone(pheromone, rho, antX, ET) # Yasmine 
+
+
+# Chooses a random initially allowed task and assigns it randomly to a processor
+    # allowedTasks -> a set of tasks with no dependency
+    # numberOfProcessors -> the number of processors available
+# returns:
+    # taskId: assigned task's id
+    # antX: dict with (processorId, [taskId])
+def initializeAnt(allowedTasks, numberOfProcessors):
+
+    taskId = random.choice(list(allowedTasks))
+    processorId = random.randint(1, numberOfProcessors)
+    antX = {
+        processorId: [taskId]
+    } 
+
+    return taskId, antX
+
+
+# Initialization test
+allowedTasks = ["A", "B", "C", "D"]
+numberOfProcessors = 10
+taskId, antX = initializeAnt(allowedTasks, numberOfProcessors)
+print(taskId)
+print(antX)
