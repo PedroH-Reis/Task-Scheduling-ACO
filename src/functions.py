a9dffde9bc164fb830d7debe8c968765a087d744
Théodore Chapuis-Chkaiban <theodore.chapuis-chkaiban@student-cs.fr>
2021-03-20 22:10:58 +0100

    SelectNextRoute implementation

--- src/functions.py
+++ src/functions.py
@@ -3,7 +3,7 @@ import random
 # PARAM: D => A dict with the information between task dependancy
 # PARAM: howManyDependancies => A dict with the information about how many dependancies has a task
 # PARAM: nextTask => The nextTask is the last task mapped
-# PARAM: allowed => The set to be atualized
+# PARAM: allowed => The set to be actualized
 def updateAllowedK(D, howManyDependancies, nextTask, allowed):
     allowed.remove(nextTask)
     for task in D[nextTask]:
@@ -49,3 +49,20 @@ def costFunction(antX, ET):
             maxET = currentET
 
     return maxET
+
+import random
+import numpy as np
+
+def selectTheNextRoute(pheromone, allowed, x= 0):
+    processors = x.keys()
+    indexes = []
+    for allowed_i in allowed:
+        for processor in processors:
+            indexes.append([allowed_i, processor])
+
+    pheromone_ind = np.array([pheromone[i[0]][i[1]] for i in indexes])
+    pheromone_ind = pheromone_ind / (np.sum(pheromone_ind))
+
+    next_task, next_processor = random.choices(indexes, pheromone_ind)
+
+    return (next_task, next_processor)
