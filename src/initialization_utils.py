eb922dbd6d2eef51b96fd68a8b84a388976835cb
Matheus Alves <matheusalves@MacBook-Pro-de-Matheus.local>
2021-03-21 10:48:26 +0100

    Creating initialization_utils and adapting main for output format

---
+++ src/initialization_utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+import json
+
+def initializeDependancyAndExecutionTimeMatrizes(jsonPath : str, numberOfProcessors: int):
+    f = open(jsonPath)
+    data = json.load(f) 
+    keys = data['nodes'].keys()
+    nTaks = len(keys)
+    D = {}
+    ET = {}
+    dependenciesCount = {}
+    intialAllowed = []
+    sum_time = 0
+    for i in keys:
+        actual = data['nodes'][i]
+        dependenciesCount[i] = len(actual['Dependencies'])
+        timeSplitted = actual["Data"].split(":")
+        time = 3600*float(timeSplitted[0]) + 60*float(timeSplitted[1]) + float(timeSplitted[2])
+        sum_time += time
+        ET[i] = [time for i in range(numberOfProcessors)]
+        if(dependenciesCount[i] == 0):
+            intialAllowed.append(i)
+        for j in actual['Dependencies']:
+            if(j not in D):
+                D[j] = []
+            D[j].append(i)
+    
+
+    eta = sum_time/nTaks
+    initializionPheromone = (1/eta)*np.ones((nTaks,nTaks))
+    
+    initializionPheromone = {i: {j:(1/eta) for j in keys} for i in keys}
+    eta = {i: {j:eta for j in keys} for i in keys}
+    # print(eta)
+
+    return D, dependenciesCount ,ET, intialAllowed, nTaks,  eta, initializionPheromone
+
+
+def initializePheromone(ET):
+    pass
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from functions import *
+from initialization_utils import initializeDependancyAndExecutionTimeMatrizes
 
 def exec(jsonPath, numberOfAnts, numberOfProcessors, iterMax, alpha, beta, rho):
-	D, howManyDependancies, ET, initialAllowed, numberOfTasks = initializeDependancyAndExecutionTimeMatrizes(jsonPath) # Matheus
-	eta = definingEta(ET) #Matheus
-	pheromone = initializePheromone(ET) # Matheus
+	D, howManyDependancies, ET, initialAllowed, numberOfTasks, eta, pheromone  = initializeDependancyAndExecutionTimeMatrizes(jsonPath, numberOfProcessors) # Matheus
+	# eta = definingEta(ET) #Matheus
+	# pheromone = initializePheromone(ET) # Matheus
 	
 	# The solution
 	x = {}
