083ad751143a2a3b2598351be5d9dc314edbe410
Yass403 <76566030+Yass403@users.noreply.github.com>
2021-03-24 14:47:25 +0100

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -103,12 +103,12 @@ def selectTheNextRoute(eta, alpha, pheromone, beta, allowed, antX, taskInfos, pr
     
     return (nextTask, nextProcessor)
 
-def _update_pheromone(pheromone, rho, allowed, ET,L,antX,numberofTasks,Q):
+def _update_pheromone(pheromone, rho, allowed, ET,L,antX,numberofTasks):
     pheromone_delta = [[0 for j in range( numberOfTasks)] for i in range(numberOfTasks)]
     for i in ET:
         for j in allowed:
             if i in antX and j in antX:
-                pheromone_delta[i][j] = Q/L
+                pheromone_delta[i][j] = 1/L
             else:
                 pheromone_delta[i][j] = 0
         pheromone[i][j] *= 1-rho
