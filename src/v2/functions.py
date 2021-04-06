72e232329b54c53892bbc4ebe5c9f69da244ed32
Pedro Henrique Carvalho dos Reis <phenrique.carvalho.reis@gmail.com>
2021-04-06 22:04:02 +0200

    new version

--- src/v2/functions.py
+++ src/v2/functions.py
@@ -47,22 +47,49 @@ def initializeOutputVariables(numberOfProcessors, numberOfTasks):
     processorInfo = [processorInfoDict for i in range(numberOfProcessors)]
     taskInfo = [taskInfoDict for i in range(numberOfTasks)]
 
-    return mapInfo, processorInfo, taskInfo
+    x = np.zeros((numberOfProcessors, numberOfTasks))
 
-def selectTheNextMap(allowedTasks, processorInfo, strategy = "RTRP"):
+    return x, mapInfo, processorInfo, taskInfo
+
+def initializeAnt(allowedTasks, numberOfProcessors):
     nextTask = random.choice(list(allowedTasks))
-    nextProcessor = 0
-
-    if strategy == "RTRP":
-        nextProcessor = random.choice(range(len(processorInfo)))
-    elif strategy == "RTGP":
-        for processor in range(1, len(processorInfo)):
-            if processorInfo[processor]["endTime"] < processorInfo[nextProcessor]["endTime"]:
-                nextProcessor = processor
-    else:
-        raise Exception("ERROR: Strategy not supported!")
+    nextProcessor = random.choice(range(numberOfProcessors))
 
     return nextTask, nextProcessor
+
+def selectTheNextMap(D, ET, allowedTasks, processorInfo, taskInfo, numberOfProcessors, numberOfTasks, pheromone, alpha, beta):
+    eta = np.zeros((numberOfProcessors, numberOfTasks))
+    for task in allowedTasks:
+        for processor in range(numberOfProcessors):
+            endTime = calculateStartAndEndTime(ET, D, task, processor, taskInfo, processorInfo)[1]
+            eta[processor][task] = 1/endTime
+
+    probability = pheromone**alpha * eta**beta
+    probability = probability/np.sum(probability)
+
+    accumulatedProbability = 0
+    randomNumber = random.uniform(0, 1)
+    for task in allowedTasks:
+        for processor in range(numberOfProcessors):
+            accumulatedProbability += probability[processor][task]
+            if randomNumber < accumulatedProbability:
+                return task, processor
+
+def calculateStartAndEndTime(ET, D, task, processor, taskInfo, processorInfo):
+    startTime = 0
+    endTime = 0
+    for fatherTask in D[task]:
+        if taskInfo[fatherTask]["processor"] != None:
+            if taskInfo[fatherTask]["endTime"] > startTime:
+                startTime = taskInfo[fatherTask]["endTime"]
+
+    if processorInfo[processor]["endTime"] > startTime:
+        startTime = processorInfo[processor]["endTime"]
+
+    endTime = startTime + ET[task]
+
+    return startTime, endTime
+
 
 def updateAllowedTasks(auxD, allowedTasks, lastTaskExecuted):
     allowedTasks.remove(lastTaskExecuted)
@@ -73,18 +100,8 @@ def updateAllowedTasks(auxD, allowedTasks, lastTaskExecuted):
             if len(auxD[task]) == 0:
                 allowedTasks.add(task)
 
-def updateSolution(ET, D, nextTask, nextProcessor, mapInfo, processorInfo, taskInfo):
-    startTime = 0
-    endTime = 0
-
-    for fatherTask in D[nextTask]:
-        if taskInfo[fatherTask]["endTime"] > startTime:
-            startTime = taskInfo[fatherTask]["endTime"]
-
-    if processorInfo[nextProcessor]["endTime"] > startTime:
-        startTime = processorInfo[nextProcessor]["endTime"]
-
-    endTime = startTime + ET[nextTask]
+def updateSolution(ET, D, nextTask, nextProcessor, mapInfo, processorInfo, taskInfo, x):
+    startTime, endTime = calculateStartAndEndTime(ET, D, nextTask, nextProcessor, taskInfo, processorInfo)
 
     mapInfo.append({
         "processor": nextProcessor,
@@ -103,6 +120,8 @@ def updateSolution(ET, D, nextTask, nextProcessor, mapInfo, processorInfo, taskI
         "endTime": endTime
     }
 
+    x[nextProcessor][nextTask] = 1
+
 def costFunction(processorInfo):
     L = 0
     for processor in processorInfo:
@@ -114,4 +133,3 @@ def costFunction(processorInfo):
 def mapToTaskName(taskIdToTaskName, mapInfo):
     for allocation in mapInfo:
         allocation["task"] = taskIdToTaskName[allocation["task"]]
-        
