358de090e49b330b78ee8b54b322f2db32890e80
Matheus Alves <matheusalves@MacBook-Pro-de-Matheus.local>
2021-04-07 10:50:20 +0200

    Taking plot out 4

--- src/v2/parallel.py
+++ src/v2/parallel.py
@@ -7,9 +7,9 @@ import numpy as np
 import copy
 import time
 
-jsonName = "./data/test-1.json"
+jsonName = "test-1.json"
 
-numberOfProcessors = 10
+numberOfProcessors = 2
 numberOfIterations = 1
 numberOfAnts = 1
 
